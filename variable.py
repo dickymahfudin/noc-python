6b9ce3768c3fdcf8cc58ea63ae198df29f9ea1ca
dickymahfudin <dickymahfudin13@gmail.com>
2021-01-07 11:58:34 +0700

    add upload folder

--- variable.py
+++ variable.py
@@ -4,11 +4,13 @@ from pathlib import Path
 
 
 print('\n')
-# dotenv_path = join(dirname(__file__), '.env')
-dotenv_path = Path('.').resolve() / '.env'
+base_path = Path('.').resolve() / 'noc-python'
+# base_path = Path('.').resolve()
+dotenv_path = f"{base_path}/.env"
+upload_path = f"{base_path}/upload"
 # dotenv_path = Path('.').resolve() / 'noc-python' / '.env'
 # dotenv_path = "/home/ubuntu/noc-python/.env"
-print(dotenv_path)
+print(f"env => {dotenv_path}")
 load_dotenv(dotenv_path)
 
 
@@ -19,4 +21,6 @@ PORT_APT2 = os.environ.get("PORT_APT2")
 RASPI = os.environ.get("RASPI")
 TOKEN_100S_V1 = os.environ.get("TOKEN_100S_V1")
 TOKEN_SITE = os.environ.get("TOKEN_SITE")
+SSH_USER = os.environ.get("SSH_USER")
+SSH_PASS = os.environ.get("SSH_PASS")
 headerSite = {"Authorization": f"Bearer {TOKEN_SITE}"}
